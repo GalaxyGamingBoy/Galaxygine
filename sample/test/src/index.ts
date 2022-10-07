6eb9bfbfe902190c616aa671c0d027b742b0f3e3
GalaxyGamingBoy <xrteach@gmail.com>
2022-10-07 18:14:50 +0000

    fix: Ported code to newest version

--- sample/test/src/index.ts
+++ sample/test/src/index.ts
@@ -2,5 +2,5 @@ import { Galaxygine } from "./Galaxygine";
 
 var galaxygine: Galaxygine = new Galaxygine();
 galaxygine.setCanvasContext(galaxygine.Core.engineInitialization());
-galaxygine.Draw.drawSquare(10, 10, 20, 20, "red");
-galaxygine.Draw.drawSquareOutline(60, 10, 20, 20, "blue");
+galaxygine.Draw.drawRectangle(10, 10, 20, 20, "red");
+galaxygine.Draw.drawRectangleOutline(60, 10, 20, 20, "blue");
