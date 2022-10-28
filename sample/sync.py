6b6f10b5517ac004edddd15f3cc1e6803ec5e1c7
GalaxyGamingBoy <xrteach@gmail.com>
2022-10-28 18:39:43 +0300

    fix: Syncing the engine to the examplesis now done in the file sync python file

--- sample/sync.py
+++ sample/sync.py
@@ -1,7 +1,6 @@
 import shutil, os
 from pathlib import Path
 def sync():
-    os.chdir("sample")
     for filename in os.scandir("."):
         if filename.is_dir():
             shutil.copytree(Path("../src/").resolve(), Path(f"./{filename.name}/src/").resolve(), dirs_exist_ok=True)
