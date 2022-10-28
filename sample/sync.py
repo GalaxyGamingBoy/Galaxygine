c8a98cd681c91c7772993127440daf159d1a8756
GalaxyGamingBoy <xrteach@gmail.com>
2022-10-28 18:36:22 +0300

    feat: Added Initial Template files

---
+++ sample/sync.py
@@ -0,0 +1,9 @@
+import shutil, os
+from pathlib import Path
+def sync():
+    os.chdir("sample")
+    for filename in os.scandir("."):
+        if filename.is_dir():
+            shutil.copytree(Path("../src/").resolve(), Path(f"./{filename.name}/src/").resolve(), dirs_exist_ok=True)
+
+sync()
